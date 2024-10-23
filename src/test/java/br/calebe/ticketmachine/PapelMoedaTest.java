dfb5431c2577e9b41a494c353f0cdff0d9369250
WolWolffz <gtmelis9743@hotmail.com>
2024-10-22 23:32:06 -0300

    config de ambiente meu deus

---
+++ src/test/java/br/calebe/ticketmachine/PapelMoedaTest.java
@@ -0,0 +1,16 @@
+package br.calebe.ticketmachine;
+
+import static org.junit.Assert.*;
+
+import br.calebe.ticketmachine.core.*;
+
+import org.junit.Test;
+
+public class PapelMoedaTest {
+    @Test
+    public void GetValorTeste()
+    {
+        PapelMoeda papelMoeda = new PapelMoeda(2, 0);
+        assertEquals(2, papelMoeda.getValor());
+    }
+}
