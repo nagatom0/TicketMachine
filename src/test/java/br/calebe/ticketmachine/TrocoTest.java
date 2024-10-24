eb79ea60c03f04b0d71a6ab6b570acec32a1bbb8
nagatom0 <vinicius_nagatomo@hotmail.com>
2024-10-24 00:13:54 -0300

    troco and ticketMachine tests

--- src/test/java/br/calebe/ticketmachine/TrocoTest.java
+++ src/test/java/br/calebe/ticketmachine/TrocoTest.java
@@ -8,9 +8,21 @@ import org.junit.Test;
 
 public class TrocoTest {
 
-    @Test
-    public void teste()
-    {
-        assertTrue(true);
+    @Test(timeout = 15000)
+    public void testTrocoComValorExato() {
+        Troco troco = new Troco(186); 
+        assertEquals(1, troco.papeisMoeda[5].getQuantidade()); 
+        assertEquals(1, troco.papeisMoeda[4].getQuantidade()); 
+        assertEquals(1, troco.papeisMoeda[3].getQuantidade()); 
+        assertEquals(1, troco.papeisMoeda[2].getQuantidade()); 
+        assertEquals(1, troco.papeisMoeda[1].getQuantidade()); 
+        assertEquals(3, troco.papeisMoeda[0].getQuantidade()); 
+    }
+
+    @Test(timeout = 15000)
+    public void testIteratorHasNext() {
+        Troco troco = new Troco(186);
+        Iterator<PapelMoeda> iterator = troco.getIterator();
+        assertTrue(iterator.hasNext());
     }
 }
